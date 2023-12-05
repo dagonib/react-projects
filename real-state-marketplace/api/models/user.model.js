e998e86525913adb779a917571b2644826e6d170
dagonib <davidgonzalezibanez@gmail.com>
2023-12-05 23:25:56 +0100

    Create sign up api route

--- real-state-marketplace/api/models/user.model.js
+++ real-state-marketplace/api/models/user.model.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true,
     },
-    emai: {
+    email: {
         type: String,
         required: true,
         unique: true,
