4cdf5cf061ff4b4de308fc4331e1bb4638a5caae
dagonib <davidgonzalezibanez@gmail.com>
2023-12-06 16:46:38 +0100

    Create sign in api route

--- real-state-marketplace/api/controllers/auth.controller.js
+++ real-state-marketplace/api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -17,6 +19,32 @@ export const signup = async (req, res, next) => {
         res.status(201).json({
             message: 'User created successfully!'
         }); 
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(401, 'Invalid email or password'));
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, 'Invalid email or password'));
+
+        const { password: pass, ...rest} = validUser._doc;
+
+        const token = jwt.sign({
+            id: validUser._id
+        }, process.env.JWT_SECRET);
+
+        res
+            .cookie('access_token', token, { httpOnly: true})
+            .status(200)
+            .json(rest)
+
     } catch (error) {
         next(error);
     }
