a93fe17d12aeb0f0ca0b4384f44363cf1e0d75d0
dagonib <davidgonzalezibanez@gmail.com>
2023-12-06 09:57:34 +0100

    Create a middleware and a function to handle error

--- real-state-marketplace/api/controllers/auth.controller.js
+++ real-state-marketplace/api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
 
     const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -18,7 +19,7 @@ export const signup = async (req, res) => {
             message: 'User created successfully!'
         }); 
     } catch (error) {
-        res.status(500).json(error.message);
+        next(error);
     }
 
     
