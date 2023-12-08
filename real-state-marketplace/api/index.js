be047b6efddb822a7e8e0b2934f85246ab3a980b
dagonib <davidgonzalezibanez@gmail.com>
2023-12-08 10:17:39 +0100

    Create user update api

--- real-state-marketplace/api/index.js
+++ real-state-marketplace/api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import useRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
---
+++ real-state-marketplace/api/utils/verifyUser.js
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (req, res, next) => {
+    const token = req.cookies.access_token;
+    if (!token) return next(errorHandler(401, 'Not authorized'));
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) return next(errorHandler(401, 'Forbidden'));
+
+        req.user = user;    
+        next()
+    })
+}
