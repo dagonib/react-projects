62007845b6a4cda4f9d39be313c41f413dad7c6b
dagonib <davidgonzalezibanez@gmail.com>
2024-01-24 09:42:41 +0100

    complete update listing functionality

--- real-state-marketplace/client/src/pages/Profile.jsx
+++ real-state-marketplace/client/src/pages/Profile.jsx
@@ -241,7 +241,9 @@ export default function Profile() {
                   </Link>
                   <div className="flex flex-col items-center">
                     <button onClick={() => handleDelete(listing._id)} className='text-red-700 uppercase'>Delete</button>
-                    <button className='text-green-700 uppercase'>Edit</button>
+                    <Link to={`/update-listing/${listing._id}`}>
+                        <button  className='text-green-700 uppercase'>Edit</button>
+                    </Link>
                   </div>
               </div>
             ))
